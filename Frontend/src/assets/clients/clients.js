9035482c5cd299cea291ac26b37388e112dcb509
Thulana Dulan Jeewantha Amaranath <128315509+TwIsTeDDuLan@users.noreply.github.com>
2025-12-06 23:26:05 +0530

    feat: add initial global CSS styles, client logos, and client data management

--- Frontend/src/assets/clients/clients.js
+++ Frontend/src/assets/clients/clients.js
@@ -4,27 +4,31 @@ import Everest from './everest.webp';
 import Sierra from './sierra.svg';
 import Motherson from './mothersongroup.png';
 import Elcado from './elcado.png';
-import Acme from './ACME.png'
-import Cwm from './cwm.png'
-import Texlan from './texlan.webp'
+import Acme from './ACME.png';
+import Cwm from './cwm.png';
+import Texlan from './texlan.webp';
+import Wins from './wins.jpg';
+import Lct from './LCT.jpeg';
+import Lwm from './LWM.jpg';
+import Sky from './SKY.jpg'
 
 export const clients = [
     { name: "CBL", logo: cblLogo },
     { name: "Jat+", logo: jatLogo },
     { name: "Elcado", logo: Elcado },
     { name: "Everest", logo: Everest },
-    { name: "WinsLanka", logo: "WinsLanka" },
-    { name: "LCT Lanka", logo: "LCT+Lanka" },
-    { name: "Sky Roofing", logo: "Sky+Roofing" },
+    { name: "WinsLanka", logo: Wins },
+    { name: "LCT Lanka", logo: Lct },
+    { name: "Sky Roofing", logo: Sky },
     { name: "Motherson", logo: Motherson },
     { name: "ACME Lanka Distilleries", logo: Acme },
     { name: "Sierra Cables", logo: Sierra },
-    { name: "Lanka Weld Mesh", logo: "Lanka+Weld" },
+    { name: "Lanka Weld Mesh", logo: Lwm },
     { name: "Texlan", logo: Texlan },
     { name: "CWM", logo: Cwm },
     { name: "CBL", logo: cblLogo },
     { name: "Jat+", logo: jatLogo },
     { name: "Elcado", logo: Elcado },
     { name: "Everest", logo: Everest },
-    { name: "WinsLanka", logo: "WinsLanka" },
+    { name: "WinsLanka", logo: Wins },
 ];
