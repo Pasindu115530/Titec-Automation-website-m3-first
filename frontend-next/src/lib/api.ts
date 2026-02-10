68864989bffa80f5e2c545a950e216743ddcdb91
Pasindu115530 <pasinduudana12m2@gmail.com>
2026-02-10 13:34:50 +0530

    Base Url change

--- frontend-next/src/lib/api.ts
+++ frontend-next/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://127.0.0.1:8000',
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://api.titecautomation.lk',
     withCredentials: true,
     headers: {
         'Accept': 'application/json',
